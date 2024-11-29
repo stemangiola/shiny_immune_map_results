08e58e0a054ac320a1e722ea0cdb0b26507adcce
Jared Andrews <jared.andrews07@gmail.com>
2024-11-29 17:18:24 -0600

    add plot data tables

--- app/app.r
+++ app/app.r
@@ -26,7 +26,7 @@ male_organ_list <- list(
   "renal system" = c("kidney", "renal_cortex", "urinary_bladder"),
   "nasal, oral, and pharyngeal regions" = c("nose", "nasal_pharynx", "throat", "tongue", "salivary_gland", "parotid_gland", "submandibular_gland"),
   "integumentary system (skin)" = c("skin"),
-  "cerebral lobes and cortical areas" = c("frontal_cortex", "temporal_lobe", "prefrontal_cortex"),
+  "cerebral lobes and cortical areas" = c("brain", "frontal_cortex", "temporal_lobe", "prefrontal_cortex"),
   "endocrine system" = c("thyroid_gland", "pituitary_gland", "adrenal_gland"),
   "prostate" = c("prostate"),
   "trachea" = c("trachea"),
@@ -35,7 +35,7 @@ male_organ_list <- list(
   "oesophagus" = c("esophagus", "gastroesophageal_junction"),
   "cardiovascular system" = c("heart", "aorta", "left_ventricle", "mitral_valve", "tricuspid_valve", "atrial_appendage", "coronary_artery"),
   "adipose tissue" = c("adipose_tissue"),
-  "brainstem and cerebellar structures" = c("brain", "spinal_cord", "cerebellum", "cerebellar_hemisphere")
+  "brainstem and cerebellar structures" = c("spinal_cord", "cerebellum", "cerebellar_hemisphere")
 )
 
 female_organ_list <- list(
@@ -52,7 +52,7 @@ female_organ_list <- list(
   "renal system" = c("kidney", "renal_cortex", "urinary_bladder"),
   "nasal, oral, and pharyngeal regions" = c("nose", "nasal_pharynx", "nasal_septum", "throat", "salivary_gland", "parotid_gland", "submandibular_gland"),
   "integumentary system (skin)" = c("skin"),
-  "cerebral lobes and cortical areas" = c("frontal_cortex", "temporal_lobe", "prefrontal_cortex"),
+  "cerebral lobes and cortical areas" = c("brain", "frontal_cortex", "temporal_lobe", "prefrontal_cortex"),
   "endocrine system" = c("thyroid_gland", "pituitary_gland", "adrenal_gland"),
   "breast" = c("breast"),
   "trachea" = c("trachea"),
@@ -62,7 +62,7 @@ female_organ_list <- list(
   "female reproductive system" = c("ectocervix", "uterine_cervix", "vagina", "ovary", "uterus", "fallopian_tube", "endometrium"),
   "cardiovascular system" = c("heart", "aorta", "left_ventricle", "mitral_valve", "tricuspid_valve", "atrial_appendage", "coronary_artery"),
   "adipose tissue" = c("adipose_tissue"),
-  "brainstem and cerebellar structures" = c("brain", "spinal_cord", "cerebellum", "cerebellar_hemisphere")
+  "brainstem and cerebellar structures" = c("spinal_cord", "cerebellum", "cerebellar_hemisphere")
 )
 
 make_plot_data <- function(data, celltype, male_organ_map, female_organ_map, age = NULL, ethnicity = NULL, sex = "male") {
@@ -86,6 +86,9 @@ make_plot_data <- function(data, celltype, male_organ_map, female_organ_map, age
     }
 
     out_df$value <- out$proportion_mean[match(out_df$tissue_groups, out$tissue_groups)]
+    out_df$proportion_mean <- out$proportion_mean[match(out_df$tissue_groups, out$tissue_groups)]
+    out_df$proportion_lower <- out$proportion_lower[match(out_df$tissue_groups, out$tissue_groups)]
+    out_df$proportion_upper <- out$proportion_upper[match(out_df$tissue_groups, out$tissue_groups)]
 
     out_df
 }
@@ -101,15 +104,19 @@ ui <- navbarPage(
                 selectInput("ethnicity", "Ethnicity", choices = unique(prop_data$ethnicity_groups)),
                 selectInput("age", "Age", choices = unique(prop_data$age_bin_sex_specific))
             ),
-            mainPanel(
+            mainPanel(width = 10,
                 fluidRow(
                     column(6, 
                         h3("Male Proportions"),
-                        plotOutput("male_anatogram", width = "400px", height = "600px")
+                        plotOutput("male_anatogram", width = "400px", height = "600px"),
+                        br(),
+                        div(DTOutput("male_props"), style = "font-size:80%;")
                     ),
                     column(6,
                         h3("Female Proportions"),
-                        plotOutput("female_anatogram", width = "400px", height = "600px")
+                        plotOutput("female_anatogram", width = "400px", height = "600px"),
+                        br(),
+                        div(DTOutput("female_props"), style = "font-size:80%;")
                     )
                 )
             )
@@ -175,8 +182,36 @@ server <- function(input, output) {
         gganatogram(data = male_data(), sex = "male", fill = "value", organism = "human") + theme_void()
     })
 
+    output$male_props <- renderDT({
+        datatable(male_data(),
+            rownames = FALSE,
+            filter = "top",
+            extensions = c("Buttons"),
+            options = list(
+                search = list(regex = TRUE),
+                pageLength = 10,
+                dom = "Blfrtip",
+                buttons = c("copy", "csv", "excel", "pdf", "print")
+            )
+        ) %>% formatStyle(0, target = "row", lineHeight = "50%")
+    })
+
     output$female_anatogram <- renderPlot({
         gganatogram(data = female_data(), sex = "female", fill = "value", organism = "human") + theme_void()
+    })
+
+    output$female_props <- renderDT({
+        datatable(female_data(),
+            rownames = FALSE,
+            filter = "top",
+            extensions = c("Buttons"),
+            options = list(
+                search = list(regex = TRUE),
+                pageLength = 10,
+                dom = "Blfrtip",
+                buttons = c("copy", "csv", "excel", "pdf", "print")
+            )
+        ) %>% formatStyle(0, target = "row", lineHeight = "50%")
     })
 }
 
