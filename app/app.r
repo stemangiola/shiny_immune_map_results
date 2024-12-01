7b728592bab38a23fd559d25718b9e1ef476ddc6
Jared Andrews <jared.andrews07@gmail.com>
2024-12-01 14:28:53 -0600

    plot aesthetics

--- app/app.r
+++ app/app.r
@@ -3,6 +3,8 @@ library(gganatogram)
 library(shinyBS)
 library(shinyWidgets)
 library(DT)
+library(viridis)
+library(colourpicker)
 
 # Factors for easier table filtering
 prop_data <- readRDS("cell_proportion_for_shiny_app.rds")
@@ -13,60 +15,59 @@ prop_data$tissue_groups <- factor(prop_data$tissue_groups)
 
 # Need to connect tissue_groups with appropriate anatomical organs
 male_organ_list <- list(
-  "blood" = c("leukocyte"),
-  #"epithelium and mucosal tissues" = c("nose", "pleura", "endometrium", "throat", "nasal_pharynx", "nasal_septum", "esophagus"),
-  "epithelium and mucosal tissues" = c("pleura"),
-  "large intestine" = c("caecum", "rectum", "colon"),
-  "respiratory system" = c("lung", "bronchus", "pulmonary_valve", "diaphragm"),
-  "spleen" = c("spleen"),
-  "gastrointestinal accessory organs" = c("liver", "pancreas", "gall_bladder"),
-  "lymphatic system" = c("lymph_node", "tonsil"),
-  "bone marrow" = c("bone_marrow"),
-  "small intestine" = c("small_intestine", "ileum", "duodenum"),
-  "renal system" = c("kidney", "renal_cortex", "urinary_bladder"),
-  "nasal, oral, and pharyngeal regions" = c("nose", "nasal_pharynx", "throat", "tongue", "salivary_gland", "parotid_gland", "submandibular_gland"),
-  "integumentary system (skin)" = c("skin"),
-  "cerebral lobes and cortical areas" = c("brain", "frontal_cortex", "temporal_lobe", "prefrontal_cortex"),
-  "endocrine system" = c("thyroid_gland", "pituitary_gland", "adrenal_gland"),
-  "prostate" = c("prostate"),
-  "trachea" = c("trachea"),
-  "sensory-related structures" = c("nerve", "amygdala"),
-  "stomach" = c("stomach"),
-  "oesophagus" = c("esophagus", "gastroesophageal_junction"),
-  "cardiovascular system" = c("heart", "aorta", "left_ventricle", "mitral_valve", "tricuspid_valve", "atrial_appendage", "coronary_artery"),
-  "adipose tissue" = c("adipose_tissue"),
-  "brainstem and cerebellar structures" = c("spinal_cord", "cerebellum", "cerebellar_hemisphere")
+    "blood" = c("leukocyte"),
+    # "epithelium and mucosal tissues" = c("nose", "pleura", "endometrium", "throat", "nasal_pharynx", "nasal_septum", "esophagus"),
+    "epithelium and mucosal tissues" = c("pleura"),
+    "large intestine" = c("caecum", "rectum", "colon"),
+    "respiratory system" = c("lung", "bronchus", "pulmonary_valve", "diaphragm"),
+    "spleen" = c("spleen"),
+    "gastrointestinal accessory organs" = c("liver", "pancreas", "gall_bladder"),
+    "lymphatic system" = c("lymph_node", "tonsil"),
+    "bone marrow" = c("bone_marrow"),
+    "small intestine" = c("small_intestine", "ileum", "duodenum"),
+    "renal system" = c("kidney", "renal_cortex", "urinary_bladder"),
+    "nasal, oral, and pharyngeal regions" = c("nose", "nasal_pharynx", "throat", "tongue", "salivary_gland", "parotid_gland", "submandibular_gland"),
+    "integumentary system (skin)" = c("skin"),
+    "cerebral lobes and cortical areas" = c("brain", "frontal_cortex", "temporal_lobe", "prefrontal_cortex"),
+    "endocrine system" = c("thyroid_gland", "pituitary_gland", "adrenal_gland"),
+    "prostate" = c("prostate"),
+    "trachea" = c("trachea"),
+    "sensory-related structures" = c("nerve", "amygdala"),
+    "stomach" = c("stomach"),
+    "oesophagus" = c("esophagus", "gastroesophageal_junction"),
+    "cardiovascular system" = c("heart", "aorta", "left_ventricle", "mitral_valve", "tricuspid_valve", "atrial_appendage", "coronary_artery"),
+    "adipose tissue" = c("adipose_tissue"),
+    "brainstem and cerebellar structures" = c("spinal_cord", "cerebellum", "cerebellar_hemisphere")
 )
 
 female_organ_list <- list(
-  "blood" = c("leukocyte"),
-  #"epithelium and mucosal tissues" = c("nose", "pleura", "endometrium", "throat", "nasal_pharynx", "nasal_septum", "esophagus"),
-  "epithelium and mucosal tissues" = c("pleura"),
-  "large intestine" = c("caecum", "rectum", "colon"),
-  "respiratory system" = c("lung", "bronchus", "pulmonary_valve", "diaphragm"),
-  "spleen" = c("spleen"),
-  "gastrointestinal accessory organs" = c("liver", "pancreas", "gall_bladder"),
-  "lymphatic system" = c("lymph_node", "tonsil"),
-  "bone marrow" = c("bone_marrow"),
-  "small intestine" = c("small_intestine", "ileum", "duodenum"),
-  "renal system" = c("kidney", "renal_cortex", "urinary_bladder"),
-  "nasal, oral, and pharyngeal regions" = c("nose", "nasal_pharynx", "nasal_septum", "throat", "salivary_gland", "parotid_gland", "submandibular_gland"),
-  "integumentary system (skin)" = c("skin"),
-  "cerebral lobes and cortical areas" = c("brain", "frontal_cortex", "temporal_lobe", "prefrontal_cortex"),
-  "endocrine system" = c("thyroid_gland", "pituitary_gland", "adrenal_gland"),
-  "breast" = c("breast"),
-  "trachea" = c("trachea"),
-  "sensory-related structures" = c("nerve"),
-  "stomach" = c("stomach"),
-  "oesophagus" = c("esophagus", "gastroesophageal_junction"),
-  "female reproductive system" = c("ectocervix", "uterine_cervix", "vagina", "ovary", "uterus", "fallopian_tube", "endometrium"),
-  "cardiovascular system" = c("heart", "aorta", "left_ventricle", "mitral_valve", "tricuspid_valve", "atrial_appendage", "coronary_artery"),
-  "adipose tissue" = c("adipose_tissue"),
-  "brainstem and cerebellar structures" = c("spinal_cord", "cerebellum", "cerebellar_hemisphere")
+    "blood" = c("leukocyte"),
+    # "epithelium and mucosal tissues" = c("nose", "pleura", "endometrium", "throat", "nasal_pharynx", "nasal_septum", "esophagus"),
+    "epithelium and mucosal tissues" = c("pleura"),
+    "large intestine" = c("caecum", "rectum", "colon"),
+    "respiratory system" = c("lung", "bronchus", "pulmonary_valve", "diaphragm"),
+    "spleen" = c("spleen"),
+    "gastrointestinal accessory organs" = c("liver", "pancreas", "gall_bladder"),
+    "lymphatic system" = c("lymph_node", "tonsil"),
+    "bone marrow" = c("bone_marrow"),
+    "small intestine" = c("small_intestine", "ileum", "duodenum"),
+    "renal system" = c("kidney", "renal_cortex", "urinary_bladder"),
+    "nasal, oral, and pharyngeal regions" = c("nose", "nasal_pharynx", "nasal_septum", "throat", "salivary_gland", "parotid_gland", "submandibular_gland"),
+    "integumentary system (skin)" = c("skin"),
+    "cerebral lobes and cortical areas" = c("brain", "frontal_cortex", "temporal_lobe", "prefrontal_cortex"),
+    "endocrine system" = c("thyroid_gland", "pituitary_gland", "adrenal_gland"),
+    "breast" = c("breast"),
+    "trachea" = c("trachea"),
+    "sensory-related structures" = c("nerve"),
+    "stomach" = c("stomach"),
+    "oesophagus" = c("esophagus", "gastroesophageal_junction"),
+    "female reproductive system" = c("ectocervix", "uterine_cervix", "vagina", "ovary", "uterus", "fallopian_tube", "endometrium"),
+    "cardiovascular system" = c("heart", "aorta", "left_ventricle", "mitral_valve", "tricuspid_valve", "atrial_appendage", "coronary_artery"),
+    "adipose tissue" = c("adipose_tissue"),
+    "brainstem and cerebellar structures" = c("spinal_cord", "cerebellum", "cerebellar_hemisphere")
 )
 
 make_plot_data <- function(data, celltype, male_organ_map, female_organ_map, age = NULL, ethnicity = NULL, sex = "male") {
-    
     out <- data
     if (!is.null(age)) {
         out <- out[out$age_bin_sex_specific == age, ]
@@ -100,22 +101,41 @@ ui <- navbarPage(
     tabPanel(
         "Immune Proportions",
         sidebarLayout(
-            sidebarPanel(width = 3,
-                h3("Data Selection"),
+            sidebarPanel(
+                width = 3,
+                h4("Data Selection"),
                 selectInput("cell_type", "Cell Type", choices = unique(prop_data$cell_type_unified_ensemble)),
                 selectInput("ethnicity", "Ethnicity", choices = unique(prop_data$ethnicity_groups)),
-                sliderTextInput("age", "Age", choices = c("Infancy", "Childhood", "Adolescence", "Young Adulthood", "Middle Age", "Senior"), grid = TRUE)
-                #selectInput("age", "Age", choices = unique(prop_data$age_bin_sex_specific))
-            ),
-            mainPanel(width = 9,
+                sliderTextInput("age", "Age", choices = c("Infancy", "Childhood", "Adolescence", "Young Adulthood", "Middle Age", "Senior"), grid = TRUE),
+                # selectInput("age", "Age", choices = unique(prop_data$age_bin_sex_specific))
+                hr(),
+                h4("Plot Aesthetics"),
                 fluidRow(
-                    column(6, 
+                    column(
+                        6,
+                        colourInput("outline_colour", "Outline Colour", value = "lightgray"),
+                        prettyCheckbox("outline", "Plot Outline", value = TRUE),
+                    ),
+                    column(
+                        6,
+                        selectInput("palette", "Palette", choices = c("viridis", "magma", "plasma", "inferno", "cividis", "mako", "rocket", "turbo")),
+                        numericInput("opacity", "Opacity", value = 1, min = 0.01, max = 1, step = 0.1),
+                        prettyCheckbox("reverse", "Reverse Palette", value = FALSE)
+                    )
+                )
+            ),
+            mainPanel(
+                width = 9,
+                fluidRow(
+                    column(
+                        6,
                         h3("Male Proportions"),
                         plotOutput("male_anatogram", width = "350px", height = "500px"),
                         br(),
                         div(DTOutput("male_props"), style = "font-size:70%;")
                     ),
-                    column(6,
+                    column(
+                        6,
                         h3("Female Proportions"),
                         plotOutput("female_anatogram", width = "350px", height = "500px"),
                         br(),
@@ -161,7 +181,8 @@ server <- function(input, output) {
         ) %>% formatStyle(0, target = "row", lineHeight = "50%")
     })
 
-    male_data <- reactive({make_plot_data(prop_data,
+    male_data <- reactive({
+        make_plot_data(prop_data,
             celltype = input$cell_type,
             male_organ_map = male_organ_list,
             female_organ_map = female_organ_list,
@@ -171,7 +192,8 @@ server <- function(input, output) {
         )
     })
 
-    female_data <- reactive({make_plot_data(prop_data,
+    female_data <- reactive({
+        make_plot_data(prop_data,
             celltype = input$cell_type,
             male_organ_map = male_organ_list,
             female_organ_map = female_organ_list,
@@ -182,7 +204,15 @@ server <- function(input, output) {
     })
 
     output$male_anatogram <- renderPlot({
-        gganatogram(data = male_data(), sex = "male", fill = "value", organism = "human") + theme_void()
+        direc <- ifelse(input$reverse, -1, 1)
+
+        p <- gganatogram(
+            data = male_data(), sex = "male", fill = "value",
+            organism = "human", outline = input$outline,
+            fillOutline = input$outline_colour,
+        ) + theme_void()
+
+        p + scale_fill_viridis(option = input$palette, alpha = input$opacity, direction = direc)
     })
 
     output$male_props <- renderDT({
@@ -200,7 +230,15 @@ server <- function(input, output) {
     })
 
     output$female_anatogram <- renderPlot({
-        gganatogram(data = female_data(), sex = "female", fill = "value", organism = "human") + theme_void()
+        direc <- ifelse(input$reverse, -1, 1)
+
+        p <- gganatogram(
+            data = female_data(), sex = "female", fill = "value",
+            organism = "human", outline = input$outline,
+            fillOutline = input$outline_colour,
+        ) + theme_void()
+
+        p + scale_fill_viridis(option = input$palette, alpha = input$opacity, direction = direc)
     })
 
     output$female_props <- renderDT({
